8e00f8de90f984ed69a8d539f777e1735b2f3208
Himanshu Arora <himanshu.sac@hotmail.com>
2026-04-18 17:57:07 +0800

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -7,7 +7,7 @@
     <nav class="nav">
       <div class="wrap nav-inner">
         <a href="index.html" class="brand" aria-label="AroraMSP home">
-          <img src="uploads/arora_msp_logo_v4.svg" alt="AroraMSP" style="height:52px;">
+          <img src="uploads/arora_msp_logo_v4.svg" alt="AroraMSP" style="height:70px;">
         </a>
         <div class="nav-links" id="navLinks">
           <a href="services.html" data-page="services">services</a>
